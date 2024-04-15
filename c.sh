330c210bdfc3aba1ac6270f783fa6fedebf87e13
Taisuke Fukuno <fukuno@jig.jp>
2024-04-16 07:01:43 +0900

    Revert "add -Wno-all"
    This reverts commit 6a09b34f2c054d48bdc49e1617d77647fd54c513.

--- c.sh
+++ c.sh
@@ -1,1 +1,1 @@
-clang -Wno-all --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export=love -Wl,--allow-undefined love.c -o love.wasm
+clang --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export=love -Wl,--allow-undefined love.c -o love.wasm
--- love.c
+++ love.c
@@ -1,5 +1,8 @@
-love() {
+extern void putchar(int n);
+
+int love(int param) {
   putchar(65);
   putchar(66);
   putchar(67);
+  return 0;
 }
